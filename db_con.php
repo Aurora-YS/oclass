92270106e7c2940ddcf707cb90147731db9e1d5e
Aurora-YS <yonddoni@gmail.com>
2021-11-08 06:20:05 +0900

    dothome

--- db_con.php
+++ db_con.php
@@ -8,7 +8,7 @@
 
     /*닷홈 환경하에서 작성할 접속 조건 -> 외부호스트를 통해 세상 공개*/
     //$con = mysqli_connect("localhost", "닷홈 DB 아이디", "닷홈 DB 패스워드", "닷홈 DB 이름(=닷홈 DB 아이디)");
-    //$con = mysqli_connect("localhost", "ajw1079", "rnaqpddl#3369", "ajw1079");
+    // $con = mysqli_connect("localhost", "aurora", "비번", "aurora");
 
     //var_dump($con);
     mysqli_query($con, "SET NAMES utf8");  //sql의 언어 설정(인코딩 utf8로 설정)
